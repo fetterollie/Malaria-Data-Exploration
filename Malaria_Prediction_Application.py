59183f0051cd68bab4d65ac9eca420d21f168b20
“fetterollie” <jonathan.fetterolf@gmail.com>
2023-02-10 18:21:23 -0500

    streamlit, dataviz, model, images

---
+++ Malaria_Prediction_Application.py
@@ -0,0 +1,39 @@
+import streamlit as st
+import pandas as pd
+import numpy as np
+import joblib
+import webbrowser
+
+# not sure if needed
+from PIL import Image
+
+
+st.sidebar.image('images/jf.jpeg', use_column_width='always') # my pic
+st.sidebar.header('Jonathan B. Fetterolf')
+st.sidebar.caption('''Data Scientist based out of Alexandria, VA. Interested in complex problem solving, machine learning, statistical analysis and new technology surrounding the data world.''')
+
+st.sidebar.header('Contact Info')
+# Contact info links
+gh_button = st.sidebar.button('GitHub')
+if gh_button:    
+    webbrowser.open_new_tab('https://github.com/fetterollie')
+
+li_button = st.sidebar.button('LinkedIn')
+if li_button:    
+    webbrowser.open_new_tab('https://www.linkedin.com/in/jonathanfetterolf/')
+
+dt_button = st.sidebar.button('Blog')
+if dt_button:    
+    webbrowser.open_new_tab('https://dev.to/fetterollie')
+
+tw_button = st.sidebar.button('Twitter')
+if tw_button:    
+    webbrowser.open_new_tab('https://twitter.com/fetterollie')
+
+st.title('Malaria Prediction Application')
+st.image('images/header.jpeg', use_column_width='always')
+
+st.header('About the Project')
+st.write('''According to the latest [World Malaria Report](https://www.who.int/publications/i/item/9789240064898) published by the [World Health Organization](https://www.who.int/news-room/fact-sheets/detail/malaria), there were 247 million cases of malaria in 2021 compared to 245 million cases in 2020. The estimated number of malaria deaths stood at 619,000 in 2021 compared to 625,000 in 2020. An early diagnosis and subsequently early treatment of malaria will help doctors practicing in araea with high rates of malaria infection and malaria deaths. Four African countries accounted for over half of all malaria deaths worldwide: Nigeria (31.3%), the Democratic Republic of the Congo (12.6%), United Republic of Tanzania (4.1%) and Niger (3.9%).''')
+st.write('''The [WHO | Regional Office for Africa](https://www.afro.who.int/health-topics/malaria) recognizes that malaria is going undiagnosed and subsequently untreated in areas where the parasite is prevalent and the resources to diagnose and treat it are the lowest. The WHO wants to create a model that can accurately predict whether or not a cell from stained blood smear is infected with malaria in order to more effectively diagnose and treat malaria in the population. ''')
+st.write('''This application can save lives. According to the [CDC](https://www.cdc.gov/malaria/diagnosis_treatment/clinicians1.html#:~:text=The%20preferred%20antimalarial%20for%20interim,not%20adequate%20for%20interim%20treatment.): in an ideal situation malaria treatment should not be initiated until the diagnosis has been established by laboratory testing. “Presumptive treatment”, i.e., without prior laboratory confirmation, should be reserved for extreme circumstances, such as strong clinical suspicion of severe disease in a setting where prompt laboratory diagnosis is not available. Doctors will still be needed to take blood and provide treatments. Histologists will still be required to prepare slides and confirm the diagnoses. This technology will simply make their operations more effiecient and allow them to dianose and treat more patients.''')
---
+++ pages/3_Model_Prediction.py
@@ -0,0 +1,46 @@
+import streamlit as st
+import pandas as pd
+import numpy as np
+import pickle
+import tensorflow as tf
+import tensorflow.keras
+
+st.title('Model Prediction')
+
+@st.cache_resource()
+def load_model():
+    with st.spinner('Loading Model...'):
+        model = tf.keras.models.load_model('model5.h5')
+        return model
+    
+malaria_model = load_model()
+
+st.sidebar.title('Forms')
+form1 = st.sidebar.form(key='Input')
+
+image_upload = form1.file_uploader(label='Upload Image to Predict', type='png')
+
+form_button = form1.form_submit_button('Submit Image')
+if form_button:
+    st.text('Uploaded Image')
+    st.image(image_upload)  
+
+predict_button = st.button('Make Prediction')
+if predict_button:
+    imdim = 64
+    img = tf.keras.utils.load_img(image_upload)
+    inp_arr = tf.keras.utils.img_to_array(img) # convert to array
+    mod_arr = np.array([inp_arr]) # convert to batch structure
+
+    resize = tf.keras.layers.Resizing(imdim, imdim) # resize
+    resized = resize(mod_arr)
+
+    pred_class = None #initiate class placeholder
+    pred = malaria_model.predict(resized) #make prediction & fill pred_class
+    if pred >= 0.5:
+        pred_class = 'Uninfected'
+    else:
+        pred_class = 'Parasitized'
+    st.image(image_upload)
+    st.write(f'Prediction: {pred_class}')
+    st.write(f'Prediction Value: {pred[0][0]}')
